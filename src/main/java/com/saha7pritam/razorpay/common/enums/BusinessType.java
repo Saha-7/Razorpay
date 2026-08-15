b5b5ac194d6f984ec80e9220604975dac7fd13b2
saha-7op <saha7pritam@gmail.com>
2026-08-15 22:28:14 +0530

    Merchant Entity & Enums created

---
+++ src/main/java/com/saha7pritam/razorpay/common/enums/BusinessType.java
@@ -0,0 +1,10 @@
+package com.saha7pritam.razorpay.common.enums;
+
+public enum BusinessType {
+    LLP,
+    PRIVATE_LIMITED,
+    PUBLIC_LIMITED,
+    PROPRIETORSHIP,
+    PARTNERSHIP,
+    TRUST
+}
---
+++ src/main/java/com/saha7pritam/razorpay/common/enums/MerchantStatus.java
@@ -0,0 +1,8 @@
+package com.saha7pritam.razorpay.common.enums;
+
+public enum MerchantStatus {
+    PENDING_KYC,
+    ACTIVE,
+    INACTIVE,
+    SUSPENDED
+}
