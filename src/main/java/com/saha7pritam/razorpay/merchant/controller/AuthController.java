55f08bace239ded0a76d61dd6a4c753ed48abc95
saha-7op <saha7pritam@gmail.com>
2026-09-11 19:20:39 +0530

    Authcontroller implementation

---
+++ src/main/java/com/saha7pritam/razorpay/merchant/controller/AuthController.java
@@ -0,0 +1,27 @@
+package com.saha7pritam.razorpay.merchant.controller;
+
+import com.saha7pritam.razorpay.merchant.dto.request.MerchantSignupRequest;
+import com.saha7pritam.razorpay.merchant.dto.response.MerchantResponse;
+import com.saha7pritam.razorpay.merchant.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@AllArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    public ResponseEntity<MerchantResponse> signup(@RequestBody @Valid MerchantSignupRequest request){
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                authService.signup(request)
+        );
+    }
+}
