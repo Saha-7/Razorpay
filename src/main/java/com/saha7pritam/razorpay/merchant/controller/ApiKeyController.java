57ec78e97eee65ee84cfa051af7bb62dad105926
saha-7op <saha7pritam@gmail.com>
2026-09-14 20:28:01 +0530

    path fix

--- src/main/java/com/saha7pritam/razorpay/merchant/controller/ApiKeyController.java
+++ src/main/java/com/saha7pritam/razorpay/merchant/controller/ApiKeyController.java
@@ -30,7 +30,7 @@ public class ApiKeyController {
         return ResponseEntity.ok(apiKeyService.listByMerchant(merchantId));
     }
 
-    @DeleteMapping("/keyId")
+    @DeleteMapping("/{keyId}")
     public ResponseEntity<Void> revoke(@PathVariable UUID merchantId, @PathVariable UUID keyId){
         apiKeyService.revoke(merchantId, keyId);
         return ResponseEntity.noContent().build();
@@ -39,7 +39,7 @@ public class ApiKeyController {
 
     @PostMapping("/{keyId}/rotate")
     public ResponseEntity<ApiKeyCreateResponse> rotateKey(@PathVariable UUID merchantId, @PathVariable UUID keyId){
-        return ResponseEntity.ok(apiKeyService.rotateKey(merchantId,keyId));
+        return ResponseEntity.ok(apiKeyService.rotate(merchantId,keyId));
     }
 
 }
